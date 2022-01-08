775b9511f8ba320d5ccc3bf7fe6dc979fa084a88
StonkDragon <76126874+StonkDragon@users.noreply.github.com>
2022-01-08 20:22:29 +0100

    Fix with negative Coordinates
    checks in negative coordinates were 1 block off.
    player coordinate -0 was interpreted as 0 resulting in inaccurate results around 0 0

--- PosCheck.java
+++ PosCheck.java
@@ -11,12 +11,31 @@ class PosCheck {
     public static void main(String[] args) {
         System.out.print("Put in the coordinates of the corners: ");
         userInput = System.console().readLine().replaceAll("[^0-9, -]", "");
+        userInput = userInput.trim();
         data = userInput.split(",| ");
+        int[] dataints = new int[data.length];
+        int index = 0;
+        for (String str : data) {
+            if (str.startsWith("-")) {
+                dataints[index] = Integer.parseInt(str) - 1;
+            } else {
+                dataints[index] = Integer.parseInt(str);
+            }
+            index++;
+        }
+        index = 0;
+        int[] dataout = new int[dataints.length];
+        for (int coord : dataints) {
+            System.out.println("Coordinate: " + coord);
+            dataout[index] = coord;
+            index++;
+        }
+
         for (int i = 0; i < 3; i++) {
-            pos[i] = Math.min(Integer.parseInt(data[i]), Integer.parseInt(data[i+3]));
+            pos[i] = Math.min(dataout[i], dataout[i+3]);
         }
         for (int i = 3; i < 6; i++) {
-            pos[i] = Math.max(Integer.parseInt(data[i-3]), Integer.parseInt(data[i]));
+            pos[i] = Math.max(dataout[i-3], dataout[i]);
         }
         x = pos[0];
         y = pos[1];
